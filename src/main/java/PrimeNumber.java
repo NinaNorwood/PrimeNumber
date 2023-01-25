cbd688e171a4cdefc9b9c498b2bb223ed00cf45e
ninam <ninamichalski@gmx.net>
2023-01-24 18:34:06 -0800

    Finished small exercise

---
+++ src/main/java/PrimeNumber.java
@@ -0,0 +1,19 @@
+public class PrimeNumber {
+
+    public static void main(String[] args) {
+        int n = 3;
+        int count = 0;
+
+        for(int i = 1; i <= n; i++){
+            if(n%i == 0){
+                count++;
+            }}
+
+
+            if(count==2){
+                System.out.println("Primzahl");}
+            else{
+                System.out.println("Keine Primzahl");
+        }
+    }
+}
